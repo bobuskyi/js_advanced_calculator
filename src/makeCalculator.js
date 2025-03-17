f70ee27b687e3f7d00d04a948a85a908b87cc888
bobuskyi <y.bobuskyi@gmail.com>
2025-03-17 17:40:47 +0200

    add calculator

--- src/makeCalculator.js
+++ src/makeCalculator.js
@@ -4,7 +4,36 @@
  * @return {object}
  */
 function makeCalculator() {
-  // write code here
+  const calculator = {
+    result: 0,
+
+    add(value) {
+      this.result += value;
+    },
+    subtract(value) {
+      this.result -= value;
+    },
+    multiply(value) {
+      this.result *= value;
+    },
+    divide(value) {
+      if (value > 0) {
+        this.result /= value;
+      }
+    },
+    reset() {
+      this.result = 0;
+
+      return this;
+    },
+    operate(func, value) {
+      func.call(this, value);
+
+      return this;
+    },
+  };
+
+  return calculator;
 }
 
 module.exports = makeCalculator;
